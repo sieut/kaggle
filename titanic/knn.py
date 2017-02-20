28914bf832ea11601ac90b0f7ae39a9968258d35
Sieu Tsoi <sieut@umich.edu>
2017-02-19 22:09:33 -0500

    add reading test data

--- titanic/knn.py
+++ titanic/knn.py
@@ -9,7 +9,7 @@ def file_len(fname, skip_header_lines = 0):
             pass
     return i + 1 - skip_header_lines
 
-def read_from_csv(name_queue):
+def read_train_data(name_queue):
     reader = tf.TextLineReader(skip_header_lines = 1)
     _, csv_row = reader.read(name_queue)
     record_defaults = [[0], [-1], [-1], [-1], [""], [""], [0.0], [0], [0], [""], [0.0], [""], [""]]
@@ -23,31 +23,56 @@ def read_from_csv(name_queue):
 
     return features, survived
 
-def input_pipeline(filenames, batch_size, num_epochs=None):
-	filename_queue = tf.train.string_input_producer(
+def read_test_data(name_queue):
+    reader = tf.TextLineReader(skip_header_lines = 1)
+    _, csv_row = reader.read(name_queue)
+    record_defaults = [[0], [-1], [-1], [""], [""], [0.0], [0], [0], [""], [0.0], [""], [""]]
+    _, _, p_class, _, sex, age, sibs_sp, par_ch, _, _, _, _ = tf.decode_csv(csv_row, record_defaults=record_defaults)
+
+    sex_comp = tf.equal(sex, "male")
+    sex = 0
+    tf.cond(sex_comp, lambda: tf.add(sex, tf.constant(0)), lambda: tf.add(sex, tf.constant(1)))
+
+    features = tf.stack([p_class, sex, tf.to_int32(age), sibs_sp, par_ch])
+
+    return features
+
+def input_pipeline(filenames, batch_size, train_data, num_epochs=None):
+    filename_queue = tf.train.string_input_producer(
 		filenames, num_epochs=num_epochs, shuffle=True)
-	features, survived = read_from_csv(filename_queue)
+    min_after_dequeue = 10000
+    capacity = min_after_dequeue + 3 * batch_size
 
-	min_after_dequeue = 10000
-	capacity = min_after_dequeue + 3 * batch_size
+    if train_data:
+        features, survived = read_train_data(filename_queue)
+        features_batch, label_batch = tf.train.batch(
+        	[features, survived], batch_size=batch_size, capacity=capacity)
+        return features_batch, label_batch
+    else:
+        features = read_test_data(filename_queue)
+        features_batch = tf.train.batch(
+            [features], batch_size=batch_size, capacity=capacity)
+        return features_batch
 
-	example_batch, label_batch = tf.train.batch(
-		[features, survived], batch_size=batch_size, capacity=capacity)
-		#min_after_dequeue=min_after_dequeue)
-	return example_batch, label_batch
-
+#Loading train data
 train_batch_size = file_len(train_file_name, skip_header_lines=1)
-X, y = input_pipeline([train_file_name], train_batch_size, 1)
+Xtr, Ytr = input_pipeline([train_file_name], train_batch_size, train_data=True, num_epochs=1)
+
+#Loading test data
+test_batch_size = file_len(test_file_name, skip_header_lines=1)
+Xte = input_pipeline([test_file_name], test_batch_size, train_data=False, num_epochs=1)
+
+Xtr_print = tf.Print(Xtr, [Xtr])
 
 with tf.Session() as sess:
-    tf.initialize_local_variables().run()
-    tf.initialize_all_variables().run()
+    tf.local_variables_initializer().run()
+    tf.global_variables_initializer().run()
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
 
     while True:
         try:
-            Xtr, ytr = sess.run([X, y])
-            print (Xtr, ytr)
+            Xtr_to_print, Ytr_to_print = sess.run([Xtr, Ytr])
+            print (Xtr_to_print, Ytr_to_print)
         except tf.errors.OutOfRangeError:
             break
