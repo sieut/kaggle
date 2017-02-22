8443b9c19b869e5fa314faf8f044028124f519cf
Sieu Tsoi <sieut@umich.edu>
2017-02-22 17:55:00 -0500

    preprocess test data

---
+++ titanic/preprocess_te.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv("test.csv")
+
+def set_average_age_for_title(title):
+	idx = df.Name.str.contains(title)
+	df_contain_title = df.Age[idx]
+	df_contain_title = df_contain_title.dropna()
+	df.loc[(idx) & (df.Age.isnull()), "Age"] = df_contain_title.mean()
+
+titles = ["Mr\.", "Mrs\.", "Miss\.", "Master\."]
+
+for title in titles:
+	set_average_age_for_title(title)
+
+# Transform columns to values in range 0 - 1
+df.Age = (df.Age - df.Age.min()) / (df.Age.max() - df.Age.min())
+df.SibSp = (df.SibSp - df.SibSp.min()) / (df.SibSp.max() - df.SibSp.min())
+df.Parch = (df.Parch - df.Parch.min()) / (df.Parch.max() - df.Parch.min())
+df.Pclass = (df.Pclass - df.Pclass.min()) / (df.Pclass.max() - df.Pclass.min())
+
+df.to_csv("test_cleaned.csv")
