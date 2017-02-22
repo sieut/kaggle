4e09d1d820ad1e758bb7b35248bd48d528b085eb
Sieu Tsoi <sieut@umich.edu>
2017-02-22 08:14:01 -0500

    change preprocess data to preprocess train data, need a separate one for test

---
+++ titanic/preprocess_tr.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+
+df = pd.read_csv("train.csv")
+
+# Drop N/A values in Age, Sex and Pclass
+df = df.dropna(subset=['Age', 'Sex', 'Pclass'])
+
+# Transform columns to values in range 0 - 1
+df.Age = (df.Age - df.Age.min()) / (df.Age.max() - df.Age.min())
+df.SibSp = (df.SibSp - df.SibSp.min()) / (df.SibSp.max() - df.SibSp.min())
+df.Parch = (df.Parch - df.Parch.min()) / (df.Parch.max() - df.Parch.min())
+df.Pclass = (df.Pclass - df.Pclass.min()) / (df.Pclass.max() - df.Pclass.min())
+
+df.to_csv("train_cleaned.csv")
