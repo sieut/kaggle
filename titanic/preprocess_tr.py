6e1330a9966269ef0b491fa1d6629438e10aee8b
Sieu Tsoi <sieut@umich.edu>
2017-02-26 12:37:45 -0500

    preprocess Fare

--- titanic/preprocess_tr.py
+++ titanic/preprocess_tr.py
@@ -11,5 +11,6 @@ df.Age = (df.Age - df.Age.min()) / (df.Age.max() - df.Age.min())
 df.SibSp = (df.SibSp - df.SibSp.min()) / (df.SibSp.max() - df.SibSp.min())
 df.Parch = (df.Parch - df.Parch.min()) / (df.Parch.max() - df.Parch.min())
 df.Pclass = (df.Pclass - df.Pclass.min()) / (df.Pclass.max() - df.Pclass.min())
+df.Fare = (df.Fare - df.Fare.min()) / (df.Fare.max() - df.Fare.min())
 
 df.to_csv("train_cleaned.csv")
