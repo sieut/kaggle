56f0b0e83abb4f8c42b3028f5e24f6080443fe40
Sieu Tsoi <sieut@umich.edu>
2017-02-26 09:29:38 -0500

    log reg kinda works, but sucks, stops at ~58%

--- titanic/lgreg.py
+++ titanic/lgreg.py
@@ -4,22 +4,23 @@ import pandas as pd
 import load_data as data
 
 Xtr, Ytr = data.input_pipeline([data.train_file_name], data.train_batch_size,
-								train_data=True, num_epochs=10)
+								train_data=True, num_epochs=25)
 Xte, Xte_id = data.input_pipeline([data.test_file_name], data.test_batch_size,
-								train_data=False, num_epochs=10)
-
+								train_data=False, num_epochs=25)
 
 # Logistic Regression
-xtr = tf.placeholder(tf.float32, [714, 5])
-ytr = tf.placeholder(tf.float32, [714])
-w = tf.Variable([1.0, 1.0, 1.0, 1.0, 1.0])
+xtr = tf.placeholder(tf.float32, [None, 5])
+ytr = tf.placeholder(tf.float32, [None, 2])
+w = tf.Variable(tf.zeros([5, 2]))
+b = tf.Variable(tf.zeros([2]))
 
-z = tf.reduce_sum(tf.multiply(xtr, w), reduction_indices=1)
-h = tf.divide(tf.constant(1.0), tf.constant(1.0) + tf.exp(tf.negative(z)))
-loss = tf.reduce_mean(tf.add(tf.multiply(tf.negative(ytr), tf.log1p(h)),
-				tf.negative(tf.multiply(tf.add(tf.constant(1.0), tf.negative(ytr)), tf.log1p(tf.add(tf.constant(1.0), h))))))
+h = tf.nn.softmax(tf.matmul(xtr, w) + b)
 
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss, var_list=[w])
+loss = tf.reduce_mean(-tf.reduce_sum(ytr * tf.log(h), reduction_indices=1))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+correct_pred = tf.equal(tf.argmax(h, 1), tf.argmax(ytr, 1))
+acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 with tf.Session() as sess:
 	tf.local_variables_initializer().run()
@@ -27,15 +28,31 @@ with tf.Session() as sess:
 	coord = tf.train.Coordinator()
 	threads = tf.train.start_queue_runners(coord=coord)
 
-	while True:
-		Xtr_list = None
-		Ytr_list = None
-		Xte_list = None
-		Xte_id_list = None
+	Xtr_list = None
+	Ytr_list = None
+	Xte_list = None
+	Xte_id_list = None
+	_y = []
 
+	while True:
 		try:
 			Xtr_list, Ytr_list = sess.run([Xtr, Ytr])
 			Xte_list, Xte_id_list = sess.run([Xte, Xte_id])
 		except tf.errors.OutOfRangeError:
 			break
-		print sess.run(train_step, feed_dict={xtr: Xtr_list, ytr: Ytr_list})
+
+		_y = []
+		for i in range(len(Ytr_list)):
+			val = Ytr_list[i]
+			_y.append([val, 1 - val])
+
+		# preds = sess.run(h, feed_dict={xtr: Xtr_list, ytr: _y})
+		# acc = 0
+		# for i in range(len(Xtr_list)):
+		# 	if round(max(preds[i])) == Ytr_list[i]:
+		# 		acc = acc + 1
+		# print acc
+
+		sess.run(train_step, feed_dict={xtr: Xtr_list, ytr: _y})
+
+	print sess.run(acc, feed_dict={xtr: Xtr_list, ytr: _y})
