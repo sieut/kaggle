5baa5bffa9c0b3f48b58a05e61bf4d82b12c80c5
Sieu Tsoi <sieut@umich.edu>
2017-03-09 00:42:17 -0500

    return appropriate labels list for lgreg

--- mnist/load_data.py
+++ mnist/load_data.py
@@ -5,10 +5,16 @@ test_file_name = "test.csv"
 
 def read_tr():
 	df = pd.read_csv(train_file_name)
-	labels = df['label'].tolist()
-	pixels = []
 
+	labels = df['label'].tolist()
+	labels_ytr = []
+	for label in labels:
+		label_ytr = [0] * 10
+		label_ytr[label] = 1
+		labels_ytr.append(label_ytr)
+
+	pixels = []
 	for i in range(len(df)):
 		pixels.append(df.loc[i].drop('label').tolist())
 
-	return pixels, labels
+	return pixels, labels_ytr
