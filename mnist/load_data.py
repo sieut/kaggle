996d09ea4bd04aa27b6fe2df9399dbb9f073fd9b
Sieu Tsoi <sieut@umich.edu>
2017-03-09 00:31:13 -0500

    delete get file len in load_data

--- mnist/load_data.py
+++ mnist/load_data.py
@@ -3,12 +3,6 @@ import pandas as pd
 train_file_name = "train.csv"
 test_file_name = "test.csv"
 
-def file_len(fname, skip_header_lines = 0):
-	with open(fname) as f:
-		for i, l in enumerate(f):
-			pass
-	return i + 1 - skip_header_lines
-
 def read_tr():
 	df = pd.read_csv(train_file_name)
 	labels = df['label']
